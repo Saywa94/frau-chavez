413987a1385ea863245590a697461e733f907ea3
Saywa94 <saywachavez@gmail.com>
2023-03-26 17:16:26 -0400

    Added basic Slider that changes on click

---
+++ components/Slider.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import styles from "./Slider.module.css";
+import Image from "next/image";
+
+import { useState } from "react";
+
+type ImageInfo = {
+  filename: string;
+  country: string;
+  description: string;
+  main: boolean;
+};
+
+export default function Slider(props: { images: Array<ImageInfo> }) {
+  const [images, setImages] = useState(props.images);
+
+  function handleClick() {
+    let index = images.findIndex(({ main }) => main === true);
+    console.log(index);
+    index = index + 1 === images.length ? -1 : index;
+    const newImages = images.map((image, i) => {
+      return { ...image, main: i === index + 1 ? true : false };
+    });
+    setImages(newImages);
+  }
+
+  return (
+    <>
+      <div className={styles.slider}>
+        {images.map(({ filename, description, main }) => {
+          return (
+            <div
+              className={`${styles.slide} ${main ? styles.active : ""}`}
+              key={filename}
+            >
+              <Image
+                className={styles.image}
+                src={filename}
+                width={1920}
+                height={1080}
+                alt={description}
+              />
+            </div>
+          );
+        })}
+      </div>
+      <button onClick={handleClick}>Change</button>
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,33 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
+import Slider from "@/components/Slider";
+
+const main_images = [
+  {
+    filename: "/images/torre_eiffel.jpg",
+    country: "",
+    description: "Torre Eiffel",
+    main: false,
+  },
+  {
+    filename: "/images/new_york.jpg",
+    country: "",
+    description: "Manhathan Skyline",
+    main: true,
+  },
+  {
+    filename: "/images/brandenburger.jpg",
+    country: "",
+    description: "Brandenburger",
+    main: false,
+  },
+  {
+    filename: "/images/machupicchu.jpg",
+    country: "",
+    description: "Machu Picchu",
+    main: false,
+  },
+];
 
 export default function Home() {
   return (
@@ -13,13 +41,18 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.center}>
-          <h1>Kulturwelt Institut</h1>
-        </div>
 
-        <div className={styles.grid}>
-          <h3>A Frau Chavez project.</h3>
+      <main className={styles.main}>
+        <Slider images={main_images} />
+
+        <div className={styles.content}>
+          <div className={styles.center}>
+            <h1>Kulturwelt Institut</h1>
+          </div>
+
+          <div className={styles.grid}>
+            <h3>A Frau Chavez project.</h3>
+          </div>
         </div>
       </main>
     </>
